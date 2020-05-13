ec89d9a370bb333be757347684341f323da2109a
odanoburu <bcclaro@gmail.com>
2020-05-13 15:16:11 -0300

    * add example file
    * add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (compile-command . "stack build --pedantic --fast --copy-bins ")))
+
