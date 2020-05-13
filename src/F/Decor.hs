d203f00b041885b2158f045a58251cc2a25801c8
odanoburu <bcclaro@gmail.com>
2020-05-13 10:13:23 -0300

    * (decorate) more terms

--- src/F/Decor.hs
+++ src/F/Decor.hs
@@ -18,10 +18,11 @@ contextualize = foldr go mempty
         _ -> error "TBI - contextualize"
 
 
-(.>) :: (a -> a) -> (b, a) -> (b, a)
+(.>) :: (a -> b) -> (c, a) -> (c, b)
 -- | fill in term\/type with hole with term\/type in the pair and
 -- return it
 t_ .> (ctx, t') = (ctx, t_ t')
+
 
 decor :: Context -> Term -> (Context, Term)
 decor ctx@(Ctx _ (Sum n)) (Var fi vn _ _) =
@@ -32,14 +33,22 @@ decor ctx@(Ctx _ (Sum n)) (Var fi vn _ _) =
 decor ctx (Abs fi vn ty t) =
   let ctx' = ctx `addName` vn
   in Abs fi vn ty .> decor ctx' t
+decor ctx (App fi t1 t2) =
+  let (ctx', t1') = decor ctx t1
+  in App fi t1' .> decor ctx' t2
 decor ctx (TAbs fi tn t) =
   let ctx' = ctx `addName` tn
   in TAbs fi tn .> decor ctx' t
--- TODO: fill in all terms, so that it becomes easier to add new ones
--- (we know where to add them)
-decor ctx t = (ctx, t)
+decor ctx (TApp fi t ty) =
+  let (ctx', t') = decor ctx t
+  in TApp fi t' .> decorT ctx' ty
+decor _ TPack{} = error "decor tpack"
+decor _ TUnpack{} = error "decor tunpack"
+
 
 decorT :: Context -> Type -> (Context, Type)
+decorT ctx TyBool = (ctx, TyBool)
+decorT _ (TyId _) = error "decorT tyid"
 decorT ctx@(Ctx _ (Sum n)) (TyVar _ _ tvn) =
   (ctx, ) $ case nameToIndex () ctx tvn of
     Just tvi -> TyVar tvi n tvn
@@ -47,9 +56,9 @@ decorT ctx@(Ctx _ (Sum n)) (TyVar _ _ tvn) =
 decorT ctx (TyArr t1 t2) =
   let (ctx', t1') = decorT ctx t1
   in TyArr t1' .> decorT ctx' t2
-decorT ctx (TyAll tn ty) =
-  let ctx' = ctx `addName` tn
-  in TyAll tn .> decorT ctx' ty
--- TODO: fill in all terms, so that it becomes easier to add new
--- ones (we know where to add them)
-decorT ctx ty = (ctx, ty)
+decorT ctx (TyAll tvn ty) =
+  let ctx' = ctx `addName` tvn
+  in TyAll tvn .> decorT ctx' ty
+decorT ctx (TySome tvn ty) =
+  let ctx' = ctx `addName` tvn
+  in TySome tvn .> decorT ctx' ty
